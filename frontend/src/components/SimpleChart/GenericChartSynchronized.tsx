492678c703a9ed1b042cd09d861a4cdee3c91a43
Lorenzo <lorenzopicoli@me.com>
2024-06-15 20:31:32 -0400

    play with the idea of using portal to mark synchronized points

--- frontend/src/components/SimpleChart/GenericChartSynchronized.tsx
+++ frontend/src/components/SimpleChart/GenericChartSynchronized.tsx
@@ -4,6 +4,9 @@ import { useChartScales } from '../../charts/useChartScales'
 import { SynchronizedContext } from '../../charts/SynchronizedContext'
 import type { TooltipInPortalProps } from '@visx/tooltip/lib/hooks/useTooltipInPortal'
 import type { GenericChartProps } from './GenericChartTypes'
+import { isDateLike } from '../../utils/isDateLike'
+import { format } from 'date-fns'
+import { useTooltip } from '@visx/tooltip'
 export function GenericChartSynchronized<T>(props: {
   mainChart: GenericChartProps<T>
   secondaryCharts: GenericChartProps<T>[]
@@ -65,17 +68,33 @@ export function GenericChartSynchronized<T>(props: {
     return null
   }
 
+  const formattedX = isDateLike(nearestDatum.x)
+    ? format(nearestDatum.x, 'dd/MM HH:mm')
+    : nearestDatum.x
+  const formattedOriginalX = isDateLike(synchronizedContext?.currentDatum?.x)
+    ? format(synchronizedContext?.currentDatum?.x, 'dd/MM HH:mm')
+    : synchronizedContext?.currentDatum?.x
   return (
     <props.Tooltip
+      unstyled={true}
+      offsetLeft={0}
+      offsetTop={0}
+      applyPositionStyle={true}
       // set this to random so it correctly updates with parent bounds
       key={Math.random()}
       top={yScale.scale(nearestDatum.y)}
-      left={xScale.scale(nearestDatum.x) ?? 100000000000000}
+      left={xScale.scale(nearestDatum.x)}
     >
-      Data value{' '}
-      <strong>
-        {JSON.stringify(synchronizedContext?.currentDatum, null, 2)}
-      </strong>
+      <svg
+        width="20"
+        height="20"
+        style={{ position: 'absolute', top: -10, left: -10 }}
+      >
+        <circle cx="10" cy="10" r="10" fill="red" />
+      </svg>
+      {/* {formattedOriginalX}, {synchronizedContext?.currentDatum?.y}
+      <br />
+      {formattedX}, {nearestDatum.y} */}
     </props.Tooltip>
   )
 }
