9655ce531747134a64446cb8f21ce9343de06434
Lorenzo <lorenzo@Lorenzos-MacBook-Air.local>
2024-06-26 21:46:41 -0400

    wip heart rate reading table creation

--- backend/src/services/importers/samsungHealth/heartRate.ts
+++ backend/src/services/importers/samsungHealth/heartRate.ts
@@ -2,6 +2,11 @@ import type { DBTransaction } from '../../../db/types'
 import { BaseImporter } from '../BaseImporter'
 import { parse } from 'csv-parse'
 import * as fs from 'node:fs'
+import * as fsPromises from 'node:fs/promises'
+import {
+  heartRateReadingsTable,
+  type NewHeartRateReading,
+} from '../../../models/HeartRateReading'
 
 export class SamsungHealthHeartRateImport extends BaseImporter {
   override sourceId = 'samsung-health-export-hr-v1'
@@ -49,11 +54,51 @@ export class SamsungHealthHeartRateImport extends BaseImporter {
     logs: string[]
   }> {
     let importedCount = 0
+    const batchSize = 100
+    let entriesToSave: NewHeartRateReading[] = []
 
     if (!process.env.SAMSUNG_HEALTH_FOLDER) {
       throw new Error('Missing env var SAMSUNG_HEALTH_FOLDER')
     }
-    const parser = fs
+
+    for await (const record of this.readCsvFile()) {
+      if (!record['com.samsung.health.heart_rate.binning_data']) {
+        throw new Error('Missing binning data')
+      }
+      const binnedData = this.getBinnedData(
+        record['com.samsung.health.heart_rate.binning_data']
+      )
+      const newEntries: NewHeartRateReading[] = binnedData.map((d: any) => ({
+        ...this.formatCsvRowToRecord(d),
+        importJobId: params.placeholderJobId,
+      }))
+      entriesToSave = entriesToSave.concat(newEntries)
+
+      if (entriesToSave.length >= batchSize) {
+        await params.tx.insert(heartRateReadingsTable).values(entriesToSave)
+        importedCount += entriesToSave.length
+        entriesToSave = []
+      }
+    }
+
+    if (entriesToSave.length >= batchSize) {
+      await params.tx.insert(heartRateReadingsTable).values(entriesToSave)
+      importedCount += entriesToSave.length
+      entriesToSave = []
+    }
+    if (isFinite(1)) {
+      throw new Error('rollback')
+    }
+    // this.updateFirstAndLastEntry(new Date())
+    return {
+      importedCount,
+      apiCallsCount: 0,
+      logs: [],
+    }
+  }
+
+  private readCsvFile() {
+    return fs
       .createReadStream(
         `${process.env.SAMSUNG_HEALTH_FOLDER}/samsunghealth_lorenzopicoli_20240626170954/com.samsung.shealth.tracker.heart_rate.20240626170954.csv`
       )
@@ -67,17 +112,31 @@ export class SamsungHealthHeartRateImport extends BaseImporter {
           castDate: false,
         })
       )
-    for await (const record of parser) {
-      console.log('Record', record)
-    }
-    if (isFinite(1)) {
-      throw new Error('rollback')
-    }
-    // this.updateFirstAndLastEntry(new Date())
+  }
+
+  private formatCsvRowToRecord(
+    row: any
+  ): Omit<NewHeartRateReading, 'importJobId'> {
     return {
-      importedCount,
-      apiCallsCount: 0,
-      logs: [],
+      startTime: row['com.samsung.health.heart_rate.start_time'],
+      endTime: row['com.samsung.health.heart_rate.end_time'],
+      heartRate: row['com.samsung.health.heart_rate.heart_rate'],
+      heartRateMax: row['com.samsung.health.heart_rate.max'],
+      heartRateMin: row['com.samsung.health.heart_rate.min'],
+      timezone: row['com.samsung.health.heart_rate.timezone'],
+      comment: row['com.samsung.health.heart_rate.comment'],
+      binUuid: row['com.samsung.health.heart_rate.binning_data'],
+      dataExportId: row['com.samsung.health.heart_rate.datauuid'],
     }
+  }
+
+  private getBinnedData(binFileName: string) {
+    const fullPath = `${
+      process.env.SAMSUNG_HEALTH_FOLDER
+    }/samsunghealth_lorenzopicoli_20240626170954/jsons/com.samsung.shealth.tracker.heart_rate/${binFileName.charAt(
+      0
+    )}/${binFileName}`
+
+    return JSON.parse(fs.readFileSync(fullPath, 'utf8'))
   }
 }
