0be9733d580a52ef2b5ef8c8ef6bd2cc4805bfeb
Lorenzo <lorenzopicoli@me.com>
2024-07-14 20:37:14 -0400

    clean up heart rate import

--- backend/src/services/importers/samsungHealth/heartRate.ts
+++ backend/src/services/importers/samsungHealth/heartRate.ts
@@ -13,27 +13,66 @@ export class SamsungHealthHeartRateImport extends BaseImporter {
   override destinationTable = 'heart_rate_readings'
   override entryDateKey = 'com.samsung.health.heart_rate.create_time'
 
-  private headers = [
-    'source',
-    'tag_id',
-    'com.samsung.health.heart_rate.create_sh_ver',
-    'com.samsung.health.heart_rate.heart_beat_count',
-    'com.samsung.health.heart_rate.start_time',
-    'com.samsung.health.heart_rate.custom',
-    'com.samsung.health.heart_rate.binning_data',
-    'com.samsung.health.heart_rate.modify_sh_ver',
-    'com.samsung.health.heart_rate.update_time',
-    'com.samsung.health.heart_rate.create_time',
-    'com.samsung.health.heart_rate.max',
-    'com.samsung.health.heart_rate.min',
-    'com.samsung.health.heart_rate.time_offset',
-    'com.samsung.health.heart_rate.deviceuuid',
-    'com.samsung.health.heart_rate.comment',
-    'com.samsung.health.heart_rate.pkg_name',
-    'com.samsung.health.heart_rate.end_time',
-    'com.samsung.health.heart_rate.datauuid',
-    'com.samsung.health.heart_rate.heart_rate',
-  ]
+  private baseExportPath: string
+  private csvPath: string
+
+  private identifier = 'com.samsung.shealth.tracker.heart_rate'
+
+  private headersMap = {
+    source: 'source',
+    tagId: 'tag_id',
+    createShVer: 'com.samsung.health.heart_rate.create_sh_ver',
+    heartBeatCount: 'com.samsung.health.heart_rate.heart_beat_count',
+    startTime: 'com.samsung.health.heart_rate.start_time',
+    custom: 'com.samsung.health.heart_rate.custom',
+    binningData: 'com.samsung.health.heart_rate.binning_data',
+    modifyShVer: 'com.samsung.health.heart_rate.modify_sh_ver',
+    updateTime: 'com.samsung.health.heart_rate.update_time',
+    createTime: 'com.samsung.health.heart_rate.create_time',
+    max: 'com.samsung.health.heart_rate.max',
+    min: 'com.samsung.health.heart_rate.min',
+    timeOffset: 'com.samsung.health.heart_rate.time_offset',
+    deviceUuid: 'com.samsung.health.heart_rate.deviceuuid',
+    comment: 'com.samsung.health.heart_rate.comment',
+    packageName: 'com.samsung.health.heart_rate.pkg_name',
+    endTime: 'com.samsung.health.heart_rate.end_time',
+    dataUuid: 'com.samsung.health.heart_rate.datauuid',
+    heartRate: 'com.samsung.health.heart_rate.heart_rate',
+  }
+
+  constructor() {
+    super()
+    if (!process.env.SAMSUNG_HEALTH_FOLDER) {
+      throw new Error('Missing env var SAMSUNG_HEALTH_FOLDER')
+    }
+    // Regex to match any folder name ending with the date-time part
+    const folderRegex = /_(\d{14})$/
+    if (!process.env.SAMSUNG_HEALTH_FOLDER) {
+      throw new Error('Missing env var SAMSUNG_HEALTH_FOLDER')
+    }
+    const folders = fs
+      .readdirSync(process.env.SAMSUNG_HEALTH_FOLDER)
+      .filter((folder) => folderRegex.test(folder))
+      .map((folder) => {
+        const match = folder.match(folderRegex)
+        if (match) {
+          return {
+            name: folder,
+            date: match[1],
+          }
+        }
+        return null
+      })
+      .filter((item) => item !== null) as { name: string; date: string }[]
+
+    if (folders.length === 0) {
+      throw new Error('No Samsung Health export folders found')
+    }
+    folders.sort((a, b) => b.date.localeCompare(a.date))
+
+    this.baseExportPath = `${process.env.SAMSUNG_HEALTH_FOLDER}/${folders[0].name}`
+    this.csvPath = `${this.baseExportPath}/${this.identifier}.${folders[0].date}.csv`
+  }
 
   public async sourceHasNewData(): Promise<{
     result: boolean
@@ -62,14 +101,9 @@ export class SamsungHealthHeartRateImport extends BaseImporter {
     }
 
     for await (const record of this.readCsvFile()) {
-      const binnedData = record['com.samsung.health.heart_rate.binning_data']
-        ? this.getBinnedData(
-            record['com.samsung.health.heart_rate.binning_data']
-          )
+      const binnedData = record[this.headersMap.binningData]
+        ? this.getBinnedData(record[this.headersMap.binningData])
         : undefined
-      if (binnedData) {
-        continue
-      }
       const newEntries: NewHeartRateReading[] = binnedData?.map((d: any) => ({
         ...this.formatCsvRowToRecord(record, d),
         importJobId: params.placeholderJobId,
@@ -100,27 +134,23 @@ export class SamsungHealthHeartRateImport extends BaseImporter {
   }
 
   private readCsvFile() {
-    return fs
-      .createReadStream(
-        `${process.env.SAMSUNG_HEALTH_FOLDER}/samsunghealth_lorenzopicoli_20240627210308/com.samsung.shealth.tracker.heart_rate.20240627210308.csv`
-      )
-      .pipe(
-        parse({
-          columns: true,
-          relaxColumnCount: true,
-          fromLine: 2,
-          skipEmptyLines: true,
-          cast: true,
-          castDate: false,
-        })
-      )
+    return fs.createReadStream(this.csvPath).pipe(
+      parse({
+        columns: true,
+        relaxColumnCount: true,
+        fromLine: 2,
+        skipEmptyLines: true,
+        cast: true,
+        castDate: false,
+      })
+    )
   }
 
   private formatCsvRowToRecord(
     row: any,
     binnedData?: any
   ): Omit<NewHeartRateReading, 'importJobId'> {
-    const timezoneRaw = row['com.samsung.health.heart_rate.time_offset']
+    const timezoneRaw = row[this.headersMap.timeOffset]
     const zone = FixedOffsetZone.parseSpecifier(timezoneRaw.slice(0, -2))
     const timezone =
       zone.name.indexOf('-') > -1
@@ -128,42 +158,30 @@ export class SamsungHealthHeartRateImport extends BaseImporter {
         : zone.name.replace('+', '-')
 
     if (!zone.isValid) {
-      throw new Error(
-        'Invalid timezone: ' + row['com.samsung.health.heart_rate.time_offset']
-      )
+      throw new Error('Invalid timezone: ' + timezoneRaw)
     }
 
     return {
       startTime: binnedData
         ? DateTime.fromMillis(binnedData.start_time).toJSDate()
-        : DateTime.fromSQL(
-            row['com.samsung.health.heart_rate.start_time']
-          ).toJSDate(),
+        : DateTime.fromSQL(row[this.headersMap.startTime]).toJSDate(),
       endTime: binnedData
         ? DateTime.fromMillis(binnedData.end_time).toJSDate()
-        : DateTime.fromSQL(
-            row['com.samsung.health.heart_rate.end_time']
-          ).toJSDate(),
-      heartRate:
-        binnedData?.heart_rate ??
-        row['com.samsung.health.heart_rate.heart_rate'],
-      heartRateMax:
-        binnedData?.heart_rate_max ?? row['com.samsung.health.heart_rate.max'],
-      heartRateMin:
-        binnedData?.heart_rate_min ?? row['com.samsung.health.heart_rate.min'],
+        : DateTime.fromSQL(row[this.headersMap.endTime]).toJSDate(),
+      heartRate: binnedData?.heart_rate ?? row[this.headersMap.heartRate],
+      heartRateMax: binnedData?.heart_rate_max ?? row[this.headersMap.max],
+      heartRateMin: binnedData?.heart_rate_min ?? row[this.headersMap.min],
       timezone,
-      comment: row['com.samsung.health.heart_rate.comment'],
-      binUuid: row['com.samsung.health.heart_rate.datauuid'],
-      dataExportId: row['com.samsung.health.heart_rate.datauuid'],
+      comment: row[this.headersMap.comment],
+      binUuid: row[this.headersMap.dataUuid],
+      dataExportId: row[this.headersMap.dataUuid],
     }
   }
 
   private getBinnedData(binFileName: string) {
-    const fullPath = `${
-      process.env.SAMSUNG_HEALTH_FOLDER
-    }/samsunghealth_lorenzopicoli_20240627210308/jsons/com.samsung.shealth.tracker.heart_rate/${binFileName.charAt(
-      0
-    )}/${binFileName}`
+    const fullPath = `${this.baseExportPath}/jsons/${
+      this.identifier
+    }/${binFileName.charAt(0)}/${binFileName}`
 
     return JSON.parse(fs.readFileSync(fullPath, 'utf8'))
   }
