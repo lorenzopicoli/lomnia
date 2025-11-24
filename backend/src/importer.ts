d8e057e5006a939edad902e94a83662ca6da24fc
Lorenzo <lorenzopicoli@me.com>
2025-11-23 22:54:05 -0300

    playing with the idea of habit categories that can be extracted based on conditions

--- backend/src/importer.ts
+++ backend/src/importer.ts
@@ -1,9 +1,15 @@
 import "dotenv/config";
-import { ImporterManager } from "./importers/ImporterManager";
+import { db } from "./db/connection";
+import { habitsTable } from "./models";
+import { HabitEvaluationService } from "./services/habits/HabitRuleEvaluationService";
 
 const main = async () => {
-  const importer = new ImporterManager();
-  importer.schedule(10000);
+  // const importer = new ImporterManager();
+  // importer.schedule(10000);
+  const habits = await db.select().from(habitsTable).orderBy(habitsTable.date);
+  for (const h of habits) {
+    HabitEvaluationService.evaluateEntry(h);
+  }
   // Keep the process running
   setInterval(() => {}, 1 << 30);
 };
