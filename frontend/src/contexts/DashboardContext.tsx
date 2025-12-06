2b64dd9e33a5eb48e2698a6254fa328d88c1f47c
Lorenzo <lorenzopicoli@me.com>
2025-12-06 10:34:03 -0300

    simplify dashboard context a lot by using new lib

--- frontend/src/contexts/DashboardContext.tsx
+++ frontend/src/contexts/DashboardContext.tsx
@@ -2,7 +2,8 @@ import { useToggle } from "@mantine/hooks";
 import { subDays } from "date-fns";
 import { differenceInDays } from "date-fns/differenceInDays";
 import type React from "react";
-import { createContext, type ReactNode, useContext, useRef, useState } from "react";
+import { createContext, type ReactNode, useCallback, useContext, useMemo } from "react";
+import { StringParam, useQueryParams } from "use-query-params";
 import type { AggregationPeriod } from "../charts/types";
 
 export type Period = "week" | "month" | "year" | "all";
@@ -22,51 +23,6 @@ interface DashboardState {
 
 const DashboardContext = createContext<DashboardState | undefined>(undefined);
 
-function readParams() {
-  const params = new URLSearchParams(window.location.search);
-
-  const period = params.get("period") as Period | null;
-
-  if (period) {
-    return {
-      startDate: null,
-      endDate: null,
-      agg: (params.get("period") as AggregationPeriod) || "week",
-      period,
-    };
-  }
-
-  const start = params.get("start");
-  const end = params.get("end");
-
-  const startDate = start ? new Date(start) : subDays(new Date(), 365);
-  const endDate = end ? new Date(end) : new Date();
-
-  return {
-    startDate,
-    endDate,
-    agg: (params.get("period") as AggregationPeriod) || "week",
-    period: null,
-  };
-}
-
-function writePeriod(period: Period, aggPeriod: AggregationPeriod) {
-  const params = new URLSearchParams();
-  params.set("period", period);
-  params.set("aggPeriod", aggPeriod);
-
-  window.history.replaceState({}, "", `${window.location.pathname}?${params}`);
-}
-
-function writeStartEnd(start: Date, end: Date, aggPeriod: AggregationPeriod) {
-  const params = new URLSearchParams();
-  params.set("start", start.toISOString().slice(0, 10));
-  params.set("end", end.toISOString().slice(0, 10));
-  params.set("aggPeriod", aggPeriod);
-
-  window.history.replaceState({}, "", `${window.location.pathname}?${params}`);
-}
-
 function getPeriod(id: Period): [Date, Date] {
   const today = new Date();
   switch (id) {
@@ -82,17 +38,13 @@ function getPeriod(id: Period): [Date, Date] {
 }
 
 export const DashboardProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
-  const initial = readParams();
-
-  const initialRange: [Date, Date] = initial.period ? getPeriod(initial.period) : [initial.startDate, initial.endDate];
-
-  const didInitRef = useRef(false);
-
-  const [dateRange, setDateRangeInternal] = useState<[Date, Date]>(initialRange);
-  const [period, setPeriod] = useState<Period | null>(initial.period);
-
+  const [params, setParams] = useQueryParams({
+    start: StringParam,
+    end: StringParam,
+    aggPeriod: StringParam,
+    period: StringParam,
+  });
   const [isRearranging, toggleIsRearranging] = useToggle([false, true]);
-  const [aggPeriod, setAggPeriod] = useState<AggregationPeriod>(initial.agg);
 
   const setDateRange = (range: [Date, Date]) => {
     let aggPeriod: AggregationPeriod = "day";
@@ -107,44 +59,68 @@ export const DashboardProvider: React.FC<{ children: ReactNode }> = ({ children 
       aggPeriod = "month";
     }
 
-    setAggPeriod(aggPeriod);
-    setPeriod(null);
-    setDateRangeInternal(range);
-
-    if (didInitRef.current) {
-      writeStartEnd(range[0], range[1], aggPeriod);
-    }
+    setParams({
+      start: range[0].toISOString(),
+      end: range[1].toISOString(),
+      aggPeriod,
+      period: null,
+    });
   };
 
-  const onPeriodSelected = (id: Period) => {
-    const range = getPeriod(id);
-    let aggPeriod: AggregationPeriod = "day";
-    switch (id) {
-      case "all":
-        aggPeriod = "month";
-        break;
-      case "year":
-        aggPeriod = "week";
-        break;
-      case "month":
-        aggPeriod = "day";
-        break;
-      case "week":
-        aggPeriod = "hour";
-        break;
+  const onPeriodSelected = useCallback(
+    (id: Period) => {
+      let aggPeriod: AggregationPeriod = "day";
+      switch (id) {
+        case "all":
+          aggPeriod = "month";
+          break;
+        case "year":
+          aggPeriod = "week";
+          break;
+        case "month":
+          aggPeriod = "day";
+          break;
+        case "week":
+          aggPeriod = "hour";
+          break;
+      }
+
+      setParams({
+        start: null,
+        end: null,
+        aggPeriod: aggPeriod,
+        period: id,
+      });
+    },
+    [setParams],
+  );
+
+  const setAggPeriod = useCallback(
+    (aggPeriod: AggregationPeriod) => {
+      setParams({ aggPeriod });
+    },
+    [setParams],
+  );
+
+  const internalDateRange = useMemo(() => {
+    if (params.start && params.end) {
+      return { start: new Date(params.start), end: new Date(params.end) };
     }
 
-    setPeriod(id);
-    setAggPeriod(aggPeriod);
-    setDateRangeInternal(range);
+    if (params.period) {
+      const range = getPeriod(params.period as Period);
 
-    if (didInitRef.current) {
-      writePeriod(id, aggPeriod);
+      return { start: range[0], end: range[1] };
     }
-  };
 
-  if (!didInitRef.current) {
-    didInitRef.current = true;
+    onPeriodSelected("year");
+  }, [params.start, params.period, params.end, onPeriodSelected]);
+
+  const aggPeriod = (params.aggPeriod ?? "day") as AggregationPeriod;
+  const period = (params.period as Period) ?? null;
+
+  if (!internalDateRange) {
+    return <>Loading...</>;
   }
 
   return (
@@ -157,8 +133,8 @@ export const DashboardProvider: React.FC<{ children: ReactNode }> = ({ children 
         setDateRange,
         onPeriodSelected,
         period,
-        startDate: dateRange[0],
-        endDate: dateRange[1],
+        startDate: internalDateRange.start,
+        endDate: internalDateRange.end,
       }}
     >
       {children}
