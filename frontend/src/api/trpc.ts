ec7a9c7a726f878cd5a95378c8d1e2e296ce3bce
Lorenzo <lorenzopicoli@me.com>
2025-12-06 22:05:13 -0300

    fix batching and tabs theming

--- frontend/src/api/trpc.ts
+++ frontend/src/api/trpc.ts
@@ -1,12 +1,12 @@
 import { QueryClient } from "@tanstack/react-query";
-import { httpBatchLink } from "@trpc/client";
+import { httpLink } from "@trpc/client";
 import { createTRPCClient } from "@trpc/react-query";
 import type { inferRouterOutputs } from "@trpc/server";
 import { createTRPCOptionsProxy } from "@trpc/tanstack-react-query";
 import type { AppRouter } from "../../../backend/src/routes/router";
 
 const trpcClient = createTRPCClient<AppRouter>({
-  links: [httpBatchLink({ url: "http://localhost:3010/trpc" })],
+  links: [httpLink({ url: "http://localhost:3010/trpc" })],
 });
 
 export const queryClient = new QueryClient();
