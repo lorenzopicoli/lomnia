f5419517a9b3f81ae88da6ac831c448572af9444
Lorenzo <lorenzopicoli@me.com>
2026-01-02 18:11:03 -0300

    fix: try to fix fe nginx again

--- frontend/docker-entrypoint.sh
+++ frontend/docker-entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 set -e
 
-envsubst < /etc/nginx/nginx.conf > /etc/nginx/nginx.conf.tmp && mv /etc/nginx/nginx.conf.tmp /etc/nginx/nginx.conf
+
+envsubst '${API_URL}' < /etc/nginx/nginx.conf > /etc/nginx/nginx.conf.tmp && \
+  mv /etc/nginx/nginx.conf.tmp /etc/nginx/nginx.conf
 
 exec nginx -g "daemon off;"
